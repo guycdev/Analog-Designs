fadcff17549e475754a86d18972baf8d8d69bee9
gcWDev <cohenguy74@gmail.com>
2023-07-30 11:12:17 -0400

    Change: changed styling of the footer + moved css to modules

--- frontend/src/components/templates/Footer/Footer.jsx
+++ frontend/src/components/templates/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import plus from '../../../assets/logo.svg'
 import { ReactSVG } from "react-svg"
 import useFormSubmission from '../../hooks/useFormSubmission'
+import styles from './Footer.module.css'
+import Button from '../../elements/Button'
 
 
 export default function Footer(props) {
@@ -12,14 +14,14 @@ export default function Footer(props) {
     const { handleSubmit } = useFormSubmission(templateId);
 
     return (
-        <footer className="footer-container">
-            <div className="logos">
-                <ReactSVG src={plus} />
-                <h4>{logo}</h4>
-            </div>
-            <div className="footer-items">
-                <ul>
-                    <div className='nav-items'>
+        <footer className={styles.container}>
+            <div>
+                <div className={styles.logo}>
+                    <ReactSVG src={plus} />
+                    <h4>{logo}</h4>
+                </div>
+                <div className={styles.footerItems}>
+                    <ul>
                         <li>
                             <a href="#home">Home</a>
                         </li>
@@ -32,13 +34,13 @@ export default function Footer(props) {
                         <li>
                             <a href="#Pricing">Pricing</a>
                         </li>
-                    </div>
-                </ul>
+                    </ul>
+                </div>
+                <form onSubmit={handleSubmit} className="misc-footer" id="footer-form">
+                    <input type="email" name="email" placeholder="Enter your email" autoComplete="on" />
+                    <Button />
+                </form>
             </div>
-            <form onSubmit={handleSubmit} className="misc-footer" id="footer-form">
-                <input type="email" name="email" placeholder="Enter your email" autoComplete="on" />
-                <button type="submit" >Subscribe</button>
-            </form>
         </footer>
     )
 }
