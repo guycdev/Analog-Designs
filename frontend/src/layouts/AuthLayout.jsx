9ebd339d9ea81116c1b92cda36518f844c3f3eb8
gcWDev <cohenguy74@gmail.com>
2023-07-31 20:07:42 -0400

    Styling: changed some styling for auth page

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Card from '../components/elements/Card'
-import MiscCard from '../components/templates/MiscCard'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Card from "../components/elements/Card";
+import MiscCard from "../components/templates/MiscCard";
+import { Outlet } from "react-router-dom";
 
 export default function Login() {
-
-    return (
-        <section className="login-container">
-            <Card
-                id={0}
-            >
-                <Outlet />
-                <MiscCard />
-            </Card>
-        </section>
-    )
+  return (
+    <section className="login-container">
+      <Card id={0}>
+        <div className="auth-form">
+          <Outlet />
+        </div>
+        <MiscCard />
+      </Card>
+    </section>
+  );
 }
