2d6003e05e552ae7bc19104fc858e7266b71cae4
gcWDev <cohenguy74@gmail.com>
2023-08-17 11:22:20 -0400

    Add: added final contact form

--- frontend/src/pages/Dashboard/DashboardSupport/DashboardContact.jsx
+++ frontend/src/pages/Dashboard/DashboardSupport/DashboardContact.jsx
@@ -1,11 +1,64 @@
-import React from "react";
-import Form from "../../Home/ContactForm/Form";
-import Styles from "./DashboardSupport.module.css";
+import React, { useState } from "react";
+import styles from "./DashboardSupport.module.css";
+import ProgressBar from "../../../components/ProgressBar";
+import TextAreaInput from "../../../components/TextAreaInput";
+import TextInput from "../../../components/TextInput";
+import useFormField from "../../../hooks/useFormField";
 
 export default function DashboardContact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    description: "",
+  });
+
+  const formSetter = useFormField(setFormData);
+
   return (
-    <div className={`card ${Styles.contactContainer}`}>
-      <Form />
+    <div className={`card ${styles.contactContainer}`}>
+      <ProgressBar formData={formData} formFieldCount={4} />
+      <div className={styles.orderFormHeadings}>
+        <h2>Company Information</h2>
+        <p>Input Company / Order Information</p>
+      </div>
+      <form action="">
+        <TextInput
+          label="Name"
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name..."
+          onChange={formSetter}
+          value={formData.name}
+        />
+        <TextInput
+          label="Email"
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Email..."
+          onChange={formSetter}
+          value={formData.email}
+        />
+        <TextInput
+          label="Subject"
+          id="subject"
+          name="subject"
+          type="text"
+          placeholder="Subject..."
+          onChange={formSetter}
+          value={formData.subject}
+        />
+        <TextAreaInput
+          label="Description"
+          id="description"
+          name="description"
+          placeholder="Description..."
+          onChange={formSetter}
+          value={formData.description}
+        />
+      </form>
     </div>
   );
 }
