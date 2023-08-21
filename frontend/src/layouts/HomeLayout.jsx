196cc9dba69859826d406a3832bad6c1cd7787a4
gcWDev <cohenguy74@gmail.com>
2023-08-21 10:36:51 -0400

    Fix: removed unneccessary useeffect

--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import Nav from "../templates/Nav/Nav";
 import logo from "../assets/logo.svg";
 import Footer from "../templates/Footer/Footer";
 
 export default function HomeLayout() {
-  const location = useLocation();
-  useEffect(() => {
-    const currentPage = location.pathname;
-    const body = document.getElementsByTagName("body")[0];
-    if (currentPage === "/") {
-      body.style.backgroundSize = "contain";
-    } else {
-      body.style.backgroundSize = "cover";
-    }
-  }, [location.pathname]);
-
   return (
     <div className="main-container">
       <Nav logo={logo} name="Analog Designs" />
